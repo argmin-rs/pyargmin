5eb927deb5f9a8a51fb27863ec982073da8f3f7e
Roman Yurchak <rth.yurchak@gmail.com>
2020-03-14 19:22:07 +0100

    Add a lint CI job (#21)

--- argmin/tests/test_argmin.py
+++ argmin/tests/test_argmin.py
@@ -1,4 +1,3 @@
-
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose
@@ -6,28 +5,24 @@ from numpy.testing import assert_allclose
 import argmin
 
 
-@pytest.mark.parametrize('solver_name', ['lbfgs'])
+@pytest.mark.parametrize("solver_name", ["lbfgs"])
 def test_rosen_lbfgs(solver_name):
     # Adapted from scipy.optimize.tests.test_optimize
 
-    scipy_opt = pytest.importorskip('scipy.optimize')
+    scipy_opt = pytest.importorskip("scipy.optimize")
 
-    class Rosen():
+    class Rosen:
         def apply(self, x):
-            res = scipy_opt.rosen(x)
-            print('Roman.apply', x, res)
-            return res
+            return scipy_opt.rosen(x)
 
         def gradient(self, x):
-            res = scipy_opt.rosen_der(x)
-            print('Rosen.gradient', x, res)
-            return res
+            return scipy_opt.rosen_der(x)
 
     prob = Rosen()
 
     x0 = np.array([-1.2, 1.0])
 
-    if solver_name == 'lbfgs':
+    if solver_name == "lbfgs":
         solver = argmin.lbfgs(10)
     else:
         raise NotImplementedError
